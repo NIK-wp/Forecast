7af6048c6439dc216f8fbf738e01c3ea906b9f31
ВашÐNikita Sintyurev <sintyurevna@gmail.com>
2024-07-11 22:09:54 +0300

    requirements added

--- main.py
+++ main.py
@@ -1,34 +1,26 @@
 import requests
-import pprint
 
 
-class WeatherRequest:
-    def __init__(self, latitude, longitude, date, parameters):
-        self.latitude = latitude
-        self.longitude = longitude
-        self.date = date
-        self.parameters = parameters
+def WeatherRequest(latitude: float, longitude: float, date: str, parameters: [str]):
+    parameters_str = ','.join(parameters)
+    url = (f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}"
+           f"&hourly={parameters_str}&start={date}T00:00:00Z&end={date}T23:59:59Z")
+    response = requests.get(url)
+    if response.status_code == 200:
+        ans = response.json()
+        print(date)
+        for i in range(len(ans['hourly']['temperature_2m'])):
+            print(
+                f"{ans['hourly']['time'][i][-5:]}: температура - {ans['hourly']['temperature_2m'][i]}°C, "
+                f"осадки - {ans['hourly']['precipitation'][i]}mm,"
+                f" ветер -  {ans['hourly']['windspeed_10m'][i]}km/h")
+    else:
+        print(f'Error:{response.status_code}')
+        return None
 
-    def make_request(self):
-        parameters_str = ','.join(self.parameters)
-        url = (f"https://api.open-meteo.com/v1/forecast?latitude={self.latitude}&longitude={self.longitude}"
-               f"&hourly={parameters_str}&start={self.date}T00:00:00Z&end={self.date}T23:59:59Z")
-        response = requests.get(url)
-        if response.status_code == 200:
-            return response.json()
-        else:
-            print(f'Error:{response.status_code}')
-            return None
 
-
-latitude = 47.23571
-longitude = 39.70151
-date = '2024-07-11'
-parameters = ['temperature_2m', 'precipitation', 'windspeed_10m']
-
-obj = WeatherRequest(latitude, longitude, date, parameters)
-ans = obj.make_request()
-pprint.pprint(ans)
-print()
-print(ans['hourly']['temperature_2m'])
-print(ans['hourly']['time'])
+latitude_test = 47.23571
+longitude_test = 39.70151
+date_test = '2024-07-11'
+parameters_test = ['temperature_2m', 'precipitation', 'windspeed_10m']
+WeatherRequest(latitude_test, longitude_test, date_test, parameters_test)
