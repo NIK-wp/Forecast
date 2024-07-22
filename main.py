e67580bd8dfc518a63b15ff312f1436463627cb4
ВашÐNikita Sintyurev <sintyurevna@gmail.com>
2024-07-22 13:59:15 +0300

    Weather + table + css

--- main.py
+++ main.py
@@ -1,26 +1,54 @@
+from flask import Flask, jsonify, request, render_template, send_from_directory
 import requests
 
+app = Flask(__name__)
 
-def WeatherRequest(latitude: float, longitude: float, date: str, parameters: [str]):
+
+def get_weather_data(latitude: float, longitude: float, date: str, parameters: list):
     parameters_str = ','.join(parameters)
     url = (f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}"
            f"&hourly={parameters_str}&start={date}T00:00:00Z&end={date}T23:59:59Z")
+
     response = requests.get(url)
     if response.status_code == 200:
-        ans = response.json()
-        print(date)
-        for i in range(len(ans['hourly']['temperature_2m'])):
-            print(
-                f"{ans['hourly']['time'][i][-5:]}: температура - {ans['hourly']['temperature_2m'][i]}°C, "
-                f"осадки - {ans['hourly']['precipitation'][i]}mm,"
-                f" ветер -  {ans['hourly']['windspeed_10m'][i]}km/h")
+        return response.json()
     else:
-        print(f'Error:{response.status_code}')
-        return None
+        return {'error': f'API request failed with status {response.status_code}'}
 
 
-latitude_test = 47.23571
-longitude_test = 39.70151
-date_test = '2024-07-11'
-parameters_test = ['temperature_2m', 'precipitation', 'windspeed_10m']
-WeatherRequest(latitude_test, longitude_test, date_test, parameters_test)
+@app.route('/weather', methods=['GET'])
+def weather():
+    latitude = request.args.get('latitude', type=float)
+    longitude = request.args.get('longitude', type=float)
+    date = request.args.get('date', type=str)
+    parameters = request.args.getlist('parameters')
+
+    if not latitude or not longitude or not date or not parameters:
+        return jsonify({'Error': 'Not enough required parameters'}), 400
+
+    response = get_weather_data(latitude, longitude, date, parameters)
+
+    images = ['sun.png' if precipitation == 0 else 'cloud.png' if precipitation < 0.4 else 'storm.png' for precipitation
+              in
+              response['hourly']['precipitation']]
+    if 'error' in response:
+        return jsonify(response), 500
+    else:
+        return render_template('table.html', time=response['hourly']['time'],
+                               precipitation=response['hourly']['precipitation'],
+                               windspeed=response['hourly']['windspeed_10m'],
+                               temperature=response['hourly']['temperature_2m'],
+                               length=len(response['hourly']['time']), image_name=images), 200
+
+
+@app.route('/table')
+def table():
+    data = [
+        {"date": "10.01.2010", "temperature": 30, "city": "Moscow"},
+        {"date": "10.01.2010", "temperature": 30, "city": "SPB"},
+        {"date": "10.01.2010", "temperature": 30, "city": "Rostov"},
+    ]
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
